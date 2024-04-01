5a0a6e626d00b283c235ef0e176a74f98999eb21
Wojciech Kołłątaj <wojtekkotaj6@outlook.com>
2024-04-01 17:57:59 +0200

    Day 4

---
+++ NewChallengeApp/Program.cs
@@ -0,0 +1,22 @@
+﻿string name = "Magda";
+string sex = "Kobieta";
+
+var age = 28;
+
+if (sex == "Kobieta" && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (name == "Magda" && age == 33)
+{
+    Console.WriteLine("Magda lat 31");
+}
+else if (sex != "Kobieta" && age < 18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
+    
+    
+
+
+
